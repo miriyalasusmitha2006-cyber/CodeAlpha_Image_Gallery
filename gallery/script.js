a5a231f8190a40e6e0a94298b6a29c1796b4c0be
miriyalasusmitha2006-cyber <miriyalasusmitha2006@gmail.com>
2026-06-05 12:22:07 +0530

    Add files via upload

---
+++ gallery/script.js
@@ -0,0 +1,73 @@
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.querySelector(".lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const closeBtn = document.querySelector(".close");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let currentIndex = 0;
+
+/* Open Lightbox */
+
+galleryImages.forEach((img, index) => {
+    img.addEventListener("click", () => {
+        currentIndex = index;
+        showImage();
+        lightbox.style.display = "flex";
+    });
+});
+
+function showImage() {
+    lightboxImg.src = galleryImages[currentIndex].src;
+}
+
+/* Close */
+
+closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+/* Next */
+
+nextBtn.addEventListener("click", () => {
+    currentIndex++;
+    if (currentIndex >= galleryImages.length) {
+        currentIndex = 0;
+    }
+    showImage();
+});
+
+/* Previous */
+
+prevBtn.addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = galleryImages.length - 1;
+    }
+    showImage();
+});
+
+/* Filter Images */
+
+function filterImages(category) {
+
+    const items = document.querySelectorAll(".image");
+
+    items.forEach(item => {
+
+        if(category === "all"){
+            item.style.display = "block";
+        }
+        else{
+            item.style.display =
+            item.classList.contains(category)
+            ? "block"
+            : "none";
+        }
+    });
+
+    document.querySelectorAll(".filters button")
+        .forEach(btn => btn.classList.remove("active"));
+
+    event.target.classList.add("active");
+}
